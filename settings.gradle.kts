8f869eb8b06fd0e329ab12855aec113810b6d6ff
mariodelgado87 <mariodelgadoj87@gmail.com>
2025-12-09 17:55:25 -0500

    sdwwe
    Removed the 'org.jetbrains.kotlin.plugin.compose' plugin from the settings.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        // 🔑 CORRECCIÓN: Se ELIMINÓ 'org.jetbrains.kotlin.plugin.compose'
+
+        // Mantener KSP y Serialización (si se usan)
+        id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+        id("com.google.devtools.ksp") version "2.0.21-1.0.27"
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
